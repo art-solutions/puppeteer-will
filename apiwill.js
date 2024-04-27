cc8295dddc40540166fc551d9f96b8d4fbb3b118
art-solutions <annik.2009@gmail.com>
2024-04-27 17:47:50 +0200

    port change

--- apiwill.js
+++ apiwill.js
@@ -2,7 +2,7 @@ const express = require('express');
 const puppeteer = require('puppeteer');
 const fs = require('fs');
 const app = express();
-const port = 3000;
+const port = 3888;
 
 app.use(express.json());
 
